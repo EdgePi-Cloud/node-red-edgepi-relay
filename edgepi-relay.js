7864f5684a62e41053de9e95348f9f9c2c4ee940
iaj2 <108596576+iaj2@users.noreply.github.com>
2023-08-23 15:58:14 -0700

    input, styles, functionality

--- edgepi-relay.js
+++ edgepi-relay.js
@@ -1,5 +1,5 @@
 module.exports = function (RED) {
-    //const rpc = require('@edgepi-cloud/edgepi-rpc')
+    const rpc = require('@edgepi-cloud/edgepi-rpc')
   
     function RelayNode(config) {
       // Create new node instance with user config
@@ -8,34 +8,36 @@ module.exports = function (RED) {
       const ipc_transport = "ipc:///tmp/edgepi.pipe"
       const tcp_transport = `tcp://${config.tcpAddress}:${config.tcpPort}`
       const transport = (config.transport === "Network") ? tcp_transport : ipc_transport;
+      const energize = (config.energizeRelay) ? "closeRelay" : "openRelay"
   
 
     
-      // init new dout instance
-    //   const relay = new rpc.RelayService(transport)
+      // init new relay instance
+      const relay = new rpc.RelayService(transport)
   
-    //   if (relay){
-    //     console.info("Relay node initialized on:", transport);
-    //     node.status({fill:"green", shape:"ring", text:"d-out initialized"});
-    //   }
+      if (relay){
+        console.info("Relay node initialized on:", transport);
+        node.status({fill:"green", shape:"ring", text:"d-out initialized"});
+      }
   
       // Input event listener
-    //   node.on('input', async function(msg,send,done){
-    //     node.status({fill:"green", shape:"dot", text:"input recieved"});
-    //     try{
-    //       msg.payload = response;
-    //     }
-    //     catch(error){
-    //       msg.payload = error;
-    //       console.error(error)
-    //     }
+      node.on('input', async function(msg,send,done){
+        node.status({fill:"green", shape:"dot", text:"input recieved"});
+        try{
+          const response = await relay[energize]();
+          msg.payload = response;
+        }
+        catch(error){
+          msg.payload = error;
+          console.error(error)
+        }
         
-    //     send(msg)
+        send(msg)
         
-    //     if (done) {
-    //       done();
-    //     }
-    //   });
+        if (done) {
+          done();
+        }
+      });
   
       node.on('close', (done) => {
         node.status({ fill: 'grey', shape: 'ring', text: 'relay node terminated' });
