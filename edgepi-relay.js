e3908275ea1f1089cb2d88c65e08c354545f30dc
Josiah Kievit <josiahtesfu@gmail.com>
2023-12-16 02:34:16 -0800

    Update relay inputs.

--- edgepi-relay.js
+++ edgepi-relay.js
@@ -1,44 +1,54 @@
 module.exports = function (RED) {
-    const rpc = require('@edgepi-cloud/edgepi-rpc')
-  
-    function RelayNode(config) {
-      // Create new node instance with user config
-      RED.nodes.createNode(this, config);
-      const node = this;
-      const ipc_transport = "ipc:///tmp/edgepi.pipe"
-      const tcp_transport = `tcp://${config.tcpAddress}:${config.tcpPort}`
-      const transport = (config.transport === "Network") ? tcp_transport : ipc_transport;
-      const relayState = (config.relayState)
-  
-      // init new relay instance
-      const relay = new rpc.RelayService(transport)
-  
-      if (relay){
-        console.info("Relay node initialized on:", transport);
-        node.status({fill:"green", shape:"ring", text:"relay initialized"});
-      }
-  
-      // Input event listener
-      node.on('input', async function(msg,send,done){
-        node.status({fill:"green", shape:"dot", text:"input recieved"});
-        try{
-          const response = await relay[relayState]();
-          msg.payload = response;
-        }
-        catch(error){
+  const rpc = require("@edgepi-cloud/edgepi-rpc");
+
+  function RelayNode(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+    const relayState = config.relayState;
+
+    setInitialConfigs(config).then((relay) => {
+      node.on("input", async function (msg, send, done) {
+        node.status({ fill: "green", shape: "dot", text: "input recieved" });
+        try {
+          const input = msg.payload === "open" ? "openRelay" : "closeRelay";
+          msg.payload = await relay[input]();
+        } catch (error) {
           msg.payload = error;
-          console.error(error)
+          console.error(error);
         }
-        
-        send(msg)
-        
+        send(msg);
         if (done) {
           done();
         }
       });
-  
+    });
+
+    async function setInitialConfigs(config) {
+      const ipc_transport = "ipc:///tmp/edgepi.pipe";
+      const tcp_transport = `tcp://${config.tcpAddress}:${config.tcpPort}`;
+      const transport =
+        config.transport === "Network" ? tcp_transport : ipc_transport;
+
+      try {
+        const relay = new rpc.RelayService(transport);
+        console.info("Relay node initialized on:", transport);
+        node.status({
+          fill: "green",
+          shape: "ring",
+          text: "relay initialized",
+        });
+
+        await relay[relayState]();
+      } catch (error) {
+        console.error(error);
+        node.status({
+          fill: "red",
+          shape: "ring",
+          text: "Initialization error",
+        });
+      }
     }
-    
-    RED.nodes.registerType('relay', RelayNode);
-    
-  };
+  }
+
+  RED.nodes.registerType("relay", RelayNode);
+};
